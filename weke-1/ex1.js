b0225e3a1bdf9637d10358fee6d47b18412f3f57
ro2306 <73036969+ro2306@users.noreply.github.com>
2020-10-27 08:41:13 +0200

    Add files via upload

---
+++ weke-1/ex1.js
@@ -0,0 +1,122 @@
+// "Variables & Operators"
+// Exercise 1
+
+const maxCapacity = 14
+let surfTime = true
+let bestStudent = 13
+const purposeInLife=5
+
+ console.log(maxCapacity)
+ console.log(surfTime)
+ console.log(bestStudent)
+ console.log(purposeInLife)
+
+ // Exercise 2
+
+ const myVariable = "Let's try this "
+	
+const result = myVariable + "is the best thing ever"
+console.log(result)
+
+// Exercise 3
+
+const password = "badO23"
+const confirmPassword = "bad023"
+
+console.log(password == confirmPassword)
+
+// Exercise 4
+console.log(423*12)
+console.log(802/2)
+console.log((5+6)*3)
+
+// Exercise 5
+
+console.log((5 > 2) && false) // false
+
+console.log("knife" === "sword") // true
+
+console.log((1 < 2) || (-1 > -1) || !false) // true
+
+console.log("") // false
+
+console.log((31 % 5) == "1") // true
+
+console.log(!!true) //true
+
+console.log("5th Avenue" != "5th Avenue") // false
+
+console.log(52 !== "52") //true
+
+console.log(undefined || null) // null
+
+// Exercise 6
+let a = 3
+a = 4
+let c = 0
+let b = a
+b = 2
+a = b
+console.log (b = c)
+console.log (c = a)
+console.log (a = b)
+
+//"Conditional Statements"
+ // Exercise 1
+ 
+let numChildre = prompt("What is your numChildre?")
+ console.log(numChildre)
+ const isCareful = false 
+ console.log(isCareful)
+
+	
+ /*if  (isCareful) 
+{numChildren++
+    }
+console.log(numChildren) // will print 3
+
+*/
+
+// Exercise 2 
+
+const silverwareCount = 4
+//console.log(silverwareCount)
+if(silverwareCount % 2 != 0){
+    console.log("something missing")
+  } 
+
+  
+// Exercise 3
+let performance = "stellar"
+let salary = 11
+const goodBonus = 22
+const stellarBonus = 33
+console.log(performance)
+ console.log(salary)
+ console.log(goodBonus)
+ console.log(stellarBonus)
+
+
+  if(performance == "goodBonus") {
+    salary += goodBonus
+} else if(performance == "stellar") {
+  salary += stellarBonus
+}
+
+// Exercise 4
+const isVIP = false
+let cash = 600
+
+if(isVIP || cash > 300){
+  console.log("welcome to the club")
+
+} else  {
+   
+  console.log("You are not allowed to enter the club.")
+}
+
+// Exercise 5
+// Exercise 6
+// Exercise 7
+
+
