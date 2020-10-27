367bef5b4bc2bdb9bf7aa490fe0ff9a7eaed5dbc
ro2306 <73036969+ro2306@users.noreply.github.com>
2020-10-27 17:54:46 +0200

    Add files via upload

---
+++ Functions/Functions1.js
@@ -0,0 +1,47 @@
+
+//   "Functions"
+//Exercise 1
+const calcAge = function(x, y){
+    return (x - y)
+  }
+  
+  const sum = calcAge(2017, 1989)
+  console.log(sum) 
+
+  //Exercise 2
+
+  const Age = function(year2,year1){
+    age1=year2-year1
+   return age1
+    
+    
+     }
+  const ages = calcAge(2018, 2015)
+     console.log("You are either"+ (ages) +" or "+(ages-1) )
+
+      //Exercise 3
+      const isEven = function(num){
+        if (num%2==0)
+        return true
+        else return false}
+
+        const number=isEven(10)
+        console.log(number)
+
+        //Exercise 4
+
+        const printEven = function(arr){
+            for (let i=0; i<arr.length; i++) {
+
+           let  tf=isEven(numbers)
+             if (tf==false)
+               return arr[i]
+
+           }
+        }
+           const numbers = [1, 5, 3,10,15]
+           const nums = printEven(numbers)
+
+           console.log(nums)
+
+    
